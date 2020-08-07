b24065b7365d3a9ec31669a4d79268132cd73fc9
andyhowell8 <andyhowell8@gmail.com>
2020-08-07 15:55:55 +0100

    upgrade tezos

--- cmd/main.go
+++ cmd/main.go
@@ -11,7 +11,7 @@ var version string
 
 const (
 	tezosContainerName = "tezos"
-	tezosImage         = "docker.io/tezos/tezos-bare:v7.2"
+	tezosImage         = "docker.io/tezos/tezos-bare:v7.3"
 	tezosConfigFile    = "configs/config.json"
 
 	collectorContainerName = "collector"
